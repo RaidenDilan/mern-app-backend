5e503263a2337b2441f062242659bea0e7a13c3f
RaidenDilan <raiden18@me.com>
2020-05-12 22:01:57 +0100

    Changed env variables

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 5000;
-const dbURI = process.env.MONGODB_URI || `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0-mhonq.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`;
+const env = process.env.NODE_ENV || 'development';
+const dbURI = process.env.MONGODB_URI || `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0-mhonq.mongodb.net/${ process.env.DB_NAME }-${ env }?retryWrites=true&w=majority`;
 const secret = process.env.SECRET || 'I bet you never even look at this!';
 
 module.exports = { port, dbURI, secret,  };
