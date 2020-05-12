d8f840cc71c64e134e7c74bf1bdd9e35dfa4e03a
RaidenDilan <raiden18@me.com>
2020-05-12 20:24:20 +0100

    Changing env variables + deploying to production.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((err, req, res, next) => {
 });
 
 mongoose
-  .connect(`mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0-mhonq.mongodb.net/${ process.env.DB_NAME}?retryWrites=true&w=majority`, {
+  .connect(`mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASSWORD }@cluster0-mhonq.mongodb.net/${ process.env.DB_NAME }?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
