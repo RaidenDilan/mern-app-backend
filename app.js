dc8b9b8273d354c1022c4e9a1715f7bac5c7d351
RaidenDilan <raiden18@me.com>
2020-05-14 01:43:53 +0100

    Deploying a Combined App - Added React App Frontend.

--- app.js
+++ app.js
@@ -17,24 +17,28 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use(express.static(path.join('public')));
 
 // CORS
-app.use((req, res, next) => {
-  // '*' ---> which domain should have access
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
-  next();
-});
+// DISABLED => When serving both backend and front from the same host, we don't need cors headers.
+// app.use((req, res, next) => {
+//   // '*' ---> which domain should have access
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+//   next();
+// });
 
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 // eslint-disable-next-line
-app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
-  throw error;
-});
+app.use((req, res, next) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
+
+// app.use((req, res, next) => {
+//   const error = new HttpError('Could not find this route.', 404);
+//   throw error;
+// });
 
 app.use((err, req, res, next) => {
   if (req.file) fs.unlink(req.file.path, err => console.log(err));
---
+++ public/precache-manifest.76a41c4679f7acdad083afc2c433c446.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd59756fd82e039e3117df99d2ec451",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549ee2bafa445ed7e109",
+    "url": "/static/css/0.10eff1aa.chunk.css"
+  },
+  {
+    "revision": "08f7dc9643e9b13a83b0",
+    "url": "/static/css/4.cf8bdbd7.chunk.css"
+  },
+  {
+    "revision": "d33b0526f60587467d5b",
+    "url": "/static/css/5.314d22d6.chunk.css"
+  },
+  {
+    "revision": "68e869098165932ad6a1",
+    "url": "/static/css/6.b5f99945.chunk.css"
+  },
+  {
+    "revision": "994c840454e17a64f7ce",
+    "url": "/static/css/7.2c8f7937.chunk.css"
+  },
+  {
+    "revision": "68d9a99662a84909dda6",
+    "url": "/static/css/8.368db41f.chunk.css"
+  },
+  {
+    "revision": "badd3c186940e784ec7c",
+    "url": "/static/css/main.d7bb9742.chunk.css"
+  },
+  {
+    "revision": "549ee2bafa445ed7e109",
+    "url": "/static/js/0.faa60831.chunk.js"
+  },
+  {
+    "revision": "011824db2874ee3a0343",
+    "url": "/static/js/3.dae1c0c5.chunk.js"
+  },
+  {
+    "revision": "08f7dc9643e9b13a83b0",
+    "url": "/static/js/4.2b1b4c46.chunk.js"
+  },
+  {
+    "revision": "d33b0526f60587467d5b",
+    "url": "/static/js/5.aa1bd232.chunk.js"
+  },
+  {
+    "revision": "68e869098165932ad6a1",
+    "url": "/static/js/6.030c1de1.chunk.js"
+  },
+  {
+    "revision": "994c840454e17a64f7ce",
+    "url": "/static/js/7.4508fcbc.chunk.js"
+  },
+  {
+    "revision": "68d9a99662a84909dda6",
+    "url": "/static/js/8.c6abcac3.chunk.js"
+  },
+  {
+    "revision": "badd3c186940e784ec7c",
+    "url": "/static/js/main.713d01b1.chunk.js"
+  },
+  {
+    "revision": "41015e834e88bb31d570",
+    "url": "/static/js/runtime-main.418d5d5d.js"
+  }
+]);
