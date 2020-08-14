ef7f64123b94621b2623a6d71dba65355b912f18
RaidenDilan <raiden18@me.com>
2020-08-14 01:21:12 +0100

    database migration from mlab to atlas

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 5000;
 const env = process.env.NODE_ENV || 'development';
-const dbURI = process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-mhonq.mongodb.net/${process.env.DB_NAME}-${env}?retryWrites=true&w=majority`;
+const dbURI = process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@places.mhonq.mongodb.net/${process.env.DB_NAME}-${env}?retryWrites=true&w=majority`;
 const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true };
 
 module.exports = {
